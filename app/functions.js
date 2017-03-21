016540001d8033366be2f9d53c4e043003edac83
Mary <msibilsk@gmail.com>
2017-03-21 08:22:30 -0500

    finished a few of the function tests

--- app/functions.js
+++ app/functions.js
@@ -2,19 +2,28 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-    
+
+    return fn.apply(null, arr);
   },
 
   speak: function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   functionFunction: function(str) {
-
+    var str1 = str + ", ";
+    return function(str2){
+      return str1 + str2;
+    }
   },
 
   makeClosures: function(arr, fn) {
-
+    for(var i = 0; i < arr.length; i++){
+      arr[i] = function(){
+        return fn(arr[i]);
+      }
+    }
+    return arr;
   },
 
   partial: function(fn, str1, str2) {
